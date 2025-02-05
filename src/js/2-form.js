6159c42c8a7c6a4603ca7afd6652432fab238372
rubanAlexUa <rubashka2021@gmail.com>
2025-02-05 22:06:25 +0200

    ell :3

---
+++ src/js/2-form.js
@@ -0,0 +1,49 @@
+let formData = {
+  email: '',
+  message: '',
+};
+const storageKey = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
+
+function commonValues() {
+  if(!localStorage.getItem(storageKey)) return
+  const data = loadFromLs(storageKey);
+  console.log(data)
+  form.elements.email.value = data.email;
+  form.elements.message.value = data.message;
+}
+
+commonValues();
+
+form.addEventListener('input', e => {
+  formData.email = e.currentTarget.elements.email.value.trim();
+  formData.message = e.currentTarget.elements.message.value.trim();
+  localStorage.setItem(storageKey, JSON.stringify(formData));
+  console.log(formData);
+});
+
+form.addEventListener('submit', e => {
+  if (
+    e.currentTarget.elements.email.value.trim() === '' ||
+    e.currentTarget.elements.message.value.trim() === ''
+  ) {
+    alert('Fill please all fields');
+    return;
+  }
+  console.log(formData);
+  formData.email,
+  formData.message = '';
+  e.currentTarget.elements.message.value,
+  e.currentTarget.elements.email.value = '';
+  localStorage.removeItem(storageKey);
+});
+
+function loadFromLs(key) {
+  let data = localStorage.getItem(key);
+  try {
+    return JSON.parse(data);
+  } catch (err) {
+    console.log(err);
+    return data;
+  }
+}
